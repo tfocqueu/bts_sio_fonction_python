5aa1a1d156cb88c2c6a7f28d0048b1af0e1b7301
thomas focqueu <thomas focqueu>
2016-05-02 22:46:36 +0200

    exercice_fonction_python

---
+++ exercice8.py
@@ -0,0 +1,14 @@
+﻿# Créé par thomas, le 02/05/2016 en Python 3.2
+from lycee import*
+
+
+def compteMots(ph):
+    compteur = 0
+    for ind in range (len(ph)):
+        if (ph[ind] == " ") :
+            compteur= compteur + 1
+    compteur = compteur +1
+    return compteur
+
+print(compteMots("je suis une phrase"))
+
---
+++ exercice9.py
@@ -0,0 +1,7 @@
+﻿# Créé par thomas, le 26/04/2016 en Python 3.2
+from lycee import *
+
+def volBoite(x1=10,x2=10,x3=10):
+    return x1*x2*x3
+
+print (volBoite())
